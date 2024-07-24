a36181b9c8027b2300b0dbe94624b1670dbce57a
Danila Odintsov <danila.odintsov01@bk.ru>
2024-07-24 17:27:02 +0400

    fix vite config base path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/my-project/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/limited-trade/' : '/',
   plugins: [
     vue(),
   ],
